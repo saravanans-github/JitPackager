a039ad81968775c7469b3acf8587cd35abd616dd
Saravanan <Saravanan@hqsml-151438>
2017-06-24 00:37:49 +0800

    Initial Commit

---
+++ JitPackager/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JitPackager
+//
+//  Created by Saravanan on 6/24/17.
+//  Copyright © 2017 Saravanan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
